675117e52fc93b72ab0070982353c140611cdd32
Jacob Perry <jacobnperry@gmail.com>
2023-08-08 13:52:17 -0700

    api test

--- routes/record.mjs
+++ routes/record.mjs
@@ -4,6 +4,24 @@ import { ObjectId } from "mongodb";
 
 const router = express.Router();
 
+const client = require('drip-nodejs')({ token: '0672dc522d3b6827e4065c2866b59022', accountId: 9967522 });
+const options = { status: "active"};
+
+client.listSubscribers(options)
+  .then((response) => {
+    console.log(response)
+    // Handle `response.body`
+  })
+  .catch((error) => {
+    // Handle errors
+  });
+
+
+
+
+
+
+
 // This section will help you get a list of all the records.
 router.get("/", async (req, res) => {
   let collection = await db.collection("records");
